5988d55c689069f3075ceb2ff8580671ad2b18fa
Trictonicmp <code_shadow@hotmail.com>
2022-05-26 14:17:37 -0500

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ import './App.css';
 import Calculator from './components/Calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;

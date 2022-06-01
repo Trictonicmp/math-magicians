2ae2e01f97888073b68122a661012e56663e76a3
Trictonicmp <code_shadow@hotmail.com>
2022-06-01 00:39:02 -0500

    Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 import './App.css';
 import Nav from './components/Nav';
-// import Home from './pages/Home';
-// import Maths from './pages/Maths';
+import Home from './pages/Home';
+import Maths from './pages/Maths';
 import Quote from './pages/Quote';
 
 const App = () => (
   <div className="App">
     <Nav />
-    <Quote />
+    <Router>
+      <Routes>
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/maths" element={<Maths />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+    {/* <Quote /> */}
     {/* <Maths /> */}
     {/* <Home /> */}
     {/* <Calculator /> */}

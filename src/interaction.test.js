2ac13a766acefd6cd4f70e1181b8bc8fcac732aa
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-01 18:36:45 -0300

    tests for interactions

---
+++ src/interaction.test.js
@@ -0,0 +1,13 @@
+import {render, fireEvent, screen} from '@testing-library/react'
+import Calculator from './components/Calculator';
+
+test('check that button 8 is clicked and the imput is selected', async () => {
+  render(<Calculator />)
+  
+  fireEvent.click(screen.getByText('8'))
+  const boxes = await screen.findAllByText(/8/i);
+
+  expect(boxes[0]).toHaveClass('input');
+  expect(boxes[1]).toHaveClass('calc-button');
+});
+

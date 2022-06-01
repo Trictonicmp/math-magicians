eb1918324ebcce5e92183320b4d1a6afa421b905
Trictonicmp <code_shadow@hotmail.com>
2022-06-01 00:33:13 -0500

    Add quote component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 // import Home from './pages/Home';
-import Maths from './pages/Maths';
+// import Maths from './pages/Maths';
+import Quote from './pages/Quote';
 
 const App = () => (
   <div className="App">
     <Nav />
-    <Maths />
+    <Quote />
+    {/* <Maths /> */}
     {/* <Home /> */}
     {/* <Calculator /> */}
   </div>
---
+++ src/pages/Quote.jsx
@@ -0,0 +1,13 @@
+const Quote = () => (
+  <section className="quote">
+    <p>
+      Mathematics is not about numbers, equations, computations,
+      algorithms: it is about understanding.
+    </p>
+    <span>
+      William Paul Thurson
+    </span>
+  </section>
+);
+
+export default Quote;

2114ac273f0ab8ba994949c44cde7eef6ec5e769
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-01 19:19:01 -0300

    second test for interactions

--- src/interaction.test.js
+++ src/interaction.test.js
@@ -1,12 +1,40 @@
 import { render, fireEvent, screen } from '@testing-library/react';
+import { Router, MemoryRouter } from 'react-router';
+import renderer from 'react-test-renderer';
+import { toHaveClass } from '@testing-library/jest-dom';
 import Calculator from './components/Calculator';
+import Nav from './components/Nav';
 
-test('check that button 8 is clicked and the imput is selected', async () => {
-  render(<Calculator />);
+describe('first test', () => {
+  test('check that button 8 is clicked and the imput is selected', async () => {
+    render(<Calculator />);
 
-  fireEvent.click(screen.getByText('8'));
-  const boxes = await screen.findAllByText(/8/i);
+    fireEvent.click(screen.getByText('8'));
+    const boxes = await screen.findAllByText(/8/i);
 
-  expect(boxes[0]).toHaveClass('input');
-  expect(boxes[1]).toHaveClass('calc-button');
+    expect(boxes[0]).toHaveClass('input');
+    expect(boxes[1]).toHaveClass('calc-button');
+  });
+});
+
+describe('Nav menu', () => {
+
+  // it('navigation to calculator', () => {
+  //   const history = createMemoryHistory();
+  //   render(<Nav />);
+  //   fireEvent.click(screen.getByText('Calculator'));
+  //   expect(history.location.pathname).toBe('/maths');
+  // });
+
+  it('navigation to quote', () => {
+    const tree = renderer
+    .create(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>,
+    )
+    .toJSON();
+    fireEvent.click(screen.getByText('Quote'));
+    expect('Mathematics is not about numbers, equations, computations, algorithms: it is about understanding.').toBeInTheDocument();
+  });
 });

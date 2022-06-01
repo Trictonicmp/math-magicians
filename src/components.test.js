a1c9f2bde04fdc6a67ae3519cab025c0a4a044c9
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-01 17:47:06 -0300

    tests for calcbutton and acalculator

--- src/components.test.js
+++ src/components.test.js
@@ -3,14 +3,32 @@ import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 import Nav from './components/Nav';
+import CalcButton from './components/CalcButton';
+import Calculator from './components/Calculator';
 
-it('Footer renders correctly', () => {
+it('nav renders correctly', () => {
   const tree = renderer
     .create(
       <MemoryRouter>
         <Nav />
       </MemoryRouter>,
     )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('Calculator renders correctly', () => {
+  const tree = renderer
+    .create(<CalcButton />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('CalcButton renders correctly', () => {
+  const tree = renderer
+    .create(<Calculator />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
